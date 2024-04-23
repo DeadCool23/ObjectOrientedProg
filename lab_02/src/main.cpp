3e46c771895bb9f7264375bfb6093c739217caa8
nisuev <nisuev04@mail.ru>
2024-04-24 01:06:42 +0300

    Added tests

--- lab_02/src/main.cpp
+++ lab_02/src/main.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
-#include <algorithm>
 #include "list.h"
 #include "errors.h"
 
-#include <vector>
+#include <iostream>
+
+#define OK ~0xFFFFFFFF
+
 int main(void) {
-    List<int> h = {8, 6, 4, 2};
-    h.debug_print();
-    h *= 2;
-    std::cout << "Size: " << h.size() << std::endl;
-    for (const auto &hh : h)
-        std::cout << hh << std::endl;
-    return 0;
+    std::cout << "Здесь должны быть тесты..." << std::endl;
+    return OK;
 }
